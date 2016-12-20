e0fb25a8c17f6abef6f8ed91b195a72f18736b64
ceragon <wangxkaixwu@163.com>
2016-12-20 10:12:14 +0800

    提交忽略文件

--- src/main/java/com/elcollect/ElcollectServer.java
+++ src/main/java/com/elcollect/ElcollectServer.java
@@ -7,5 +7,6 @@ public class ElcollectServer {
 	private final static Logger log=LogManager.getLogger(ElcollectServer.class);
 	public static void main(String[] args) {
 		log.info("dsaddd");
+		log.warn("test");
 	}
 }
