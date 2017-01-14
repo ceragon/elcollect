bf6edf9c402f8368a0c1fd2a87debbf5621764ef
ceragon <wangxkaixwu@163.com>
2017-01-14 18:12:52 +0800

    调整elastic-server的目录结构

--- elcollect-server/src/main/java/com/elcolect/JfinalMain.java
+++
@@ -1,9 +0,0 @@
-package com.elcolect;
-
-import com.jfinal.core.JFinal;
-
-public class JfinalMain {
-	public static void main(String[] args) {
-		JFinal.start("src/main/webapp", 8080, "/", 1);
-	}
-}
---
+++ elcollect-server/src/main/java/com/elcolect/server/jfinal/MyJFinalConfig.java
@@ -0,0 +1,45 @@
+package com.elcolect.server.jfinal;
+
+import com.elcolect.server.jfinal.controller.HelloController;
+import com.jfinal.config.Constants;
+import com.jfinal.config.Handlers;
+import com.jfinal.config.Interceptors;
+import com.jfinal.config.JFinalConfig;
+import com.jfinal.config.Plugins;
+import com.jfinal.config.Routes;
+import com.jfinal.render.ViewType;
+
+public class MyJFinalConfig extends JFinalConfig{
+
+	@Override
+	public void configConstant(Constants me) {
+		me.setDevMode(true);
+		me.setViewType(ViewType.VELOCITY);
+	}
+
+	@Override
+	public void configHandler(Handlers arg0) {
+		
+	}
+	@Override
+	public void configInterceptor(Interceptors arg0) {
+		
+	}
+
+	@Override
+	public void configPlugin(Plugins arg0) {
+		
+	}
+	@Override
+	public void configRoute(Routes me) {
+		me.add("hello", HelloController.class);
+	}
+	@Override
+	public void afterJFinalStart() {
+	}
+	@Override
+	public void beforeJFinalStop() {
+	}
+	
+
+}
